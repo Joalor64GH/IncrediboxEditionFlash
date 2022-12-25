246c490b47ebd76dece3f73454c146cf146ad89e
Joalor64 <rhgarcon@gmail.com>
2022-12-25 12:18:59 -0500

    Update app.js

--- asset-v7/app.js
+++ asset-v7/app.js
@@ -1,101 +1,101 @@
 /* 0.5.4 2021-04-23 21:06:13 */
 var app = new function() {
-    this.name = "Jeevan", this.version = "7", this.date = "2019", this.folder = "asset-v7/", this.looptime = 6857, this.bpm = 140, this.totalframe = 342, this.nbpolo = 7, this.nbloopbonus = 3, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 27, this.recminloop = 3, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#2b0f02", this.col0 = "#fe6e23", this.col1 = "#d65a1e", this.col2 = "#ae4619", this.col3 = "#863214", this.col4 = "#551e05", this.animearray = [{
+    this.name = "The Love", this.version = "4", this.date = "2014", this.folder = "asset-v7/", this.looptime = 8e3, this.bpm = 120, this.totalframe = 384, this.nbpolo = 7, this.nbloopbonus = 1, this.bonusloopA = !0, this.bonusendloopA = !1, this.recmaxloop = 23, this.recminloop = 3, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#211101", this.col0 = "#D8A365", this.col1 = "#BA7B3D", this.col2 = "#A16224", this.col3 = "#6A3501", this.col4 = "#422101", this.animearray = [{
         name: "1_lead",
-        color: "ff6419",
-        uniqsnd: !0
-    }, {
-        name: "2_pouin",
-        color: "ff6419",
+        color: "E5A90E",
         uniqsnd: !1
     }, {
+        name: "2_pouin",
+        color: "E5A90E",
+        uniqsnd: !0
+    }, {
         name: "3_tung",
-        color: "ff6419",
+        color: "E5A90E",
         uniqsnd: !0
     }, {
         name: "4_tabla",
-        color: "ff6419",
+        color: "E5A90E",
         uniqsnd: !0
     }, {
         name: "5_tuduki",
-        color: "ff6419",
+        color: "E5A90E",
         uniqsnd: !0
     }, {
         name: "6_bass",
-        color: "e11923",
+        color: "0CB6CB",
         uniqsnd: !1
     }, {
         name: "7_bourdon",
-        color: "e11923",
+        color: "0CB6CB",
         uniqsnd: !0
     }, {
         name: "8_campan",
-        color: "e11923",
+        color: "0CB6CB",
         uniqsnd: !0
     }, {
         name: "9_kum",
-        color: "e11923",
+        color: "0CB6CB",
         uniqsnd: !1
     }, {
         name: "10_string",
-        color: "e11923",
+        color: "0CB6CB",
         uniqsnd: !1
     }, {
         name: "11_citar",
-        color: "328cd2",
-        uniqsnd: !0
+        color: "F42828",
+        uniqsnd: !1
     }, {
         name: "12_guit",
-        color: "328cd2",
-        uniqsnd: !0
+        color: "F42828",
+        uniqsnd: !1
     }, {
         name: "13_tromp",
-        color: "328cd2",
+        color: "F42828",
         uniqsnd: !1
     }, {
         name: "14_taoung",
-        color: "328cd2",
+        color: "F42828",
         uniqsnd: !0
     }, {
         name: "15_sifle",
-        color: "328cd2",
+        color: "F42828",
         uniqsnd: !1
     }, {
         name: "16_djindr",
-        color: "32b441",
-        uniqsnd: !0
+        color: "787DFF",
+        uniqsnd: !1
     }, {
         name: "17_djinga",
-        color: "32b441",
-        uniqsnd: !0
+        color: "787DFF",
+        uniqsnd: !1
     }, {
         name: "18_djinta",
-        color: "32b441",
+        color: "787DFF",
         uniqsnd: !0
     }, {
         name: "19_jeevan",
-        color: "32b441",
+        color: "787DFF",
         uniqsnd: !1
     }, {
         name: "20_yaha",
-        color: "32b441",
+        color: "787DFF",
         uniqsnd: !1
     }], this.bonusarray = [{
-        name: "Kofitez",
+        name: "Baby",
         src: "v7-b1-kofitez-hb.mp4",
-        code: "1,2,4,16,17",
+        code: "1,2,6,11,17",
         sound: "bonus_kofitez",
         aspire: "aspire_kofitez"
     }, {
-        name: "Sapna",
+        name: "Follow",
         src: "v7-b2-sapna-hb.mp4",
-        code: "1,3,6,14,15",
+        code: "1,6,8,12,18",
         sound: "bonus_sapna",
         aspire: "aspire_sapna"
     }, {
-        name: "Kabikabi",
+        name: "Eagle",
         src: "v7-b3-kabikabi-hb.mp4",
-        code: "7,8,10,11,19",
+        code: "1,3,6,13,16",
         sound: "bonus_kabikabi",
         aspire: "aspire_kabikabi"
     }], this.unlockerarray = [];
