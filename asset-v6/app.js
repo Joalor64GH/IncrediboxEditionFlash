a9047dce7a378599fd970c3516cc1530dc812c92
Joalor64 <rhgarcon@gmail.com>
2022-12-25 12:01:24 -0500

    Update app.js

--- asset-v6/app.js
+++ asset-v6/app.js
@@ -1,99 +1,99 @@
 /* 0.5.4 2021-04-23 21:06:13 */
 var app = new function() {
-    this.name = "Alive", this.version = "6", this.date = "2018", this.folder = "asset-v6/", this.looptime = 7111, this.bpm = 135, this.totalframe = 342, this.nbpolo = 7, this.nbloopbonus = 3, this.bonusloopA = !1, this.bonusendloopA = !1, this.recmaxloop = 26, this.recminloop = 3, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#120521", this.col0 = "#a07dfa", this.col1 = "#825fd2", this.col2 = "#5f3ca0", this.col3 = "#371464", this.col4 = "#230a41", this.animearray = [{
+    this.name = "Little Miss Christmas", this.version = "2", this.date = "2012", this.folder = "asset-v6/", this.looptime = 4800, this.bpm = 100, this.totalframe = 232, this.nbpolo = 7, this.nbloopbonus = 4, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 38, this.recminloop = 5, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#1c0d0d", this.col0 = "#EB4B4B", this.col1 = "#C32D2D", this.col2 = "#9B2323", this.col3 = "#691E1E", this.col4 = "#371919", this.animearray = [{
         name: "1_kick",
-        color: "1e96be",
-        uniqsnd: !0
+        color: "828282",
+        uniqsnd: !1
     }, {
         name: "2_snare",
-        color: "1e96be",
-        uniqsnd: !0
+        color: "828282",
+        uniqsnd: !1
     }, {
         name: "3_kanye",
-        color: "1e96be",
+        color: "828282",
         uniqsnd: !0
     }, {
         name: "4_tuctuc",
-        color: "1e96be",
-        uniqsnd: !0
+        color: "828282",
+        uniqsnd: !1
     }, {
         name: "5_break",
-        color: "1e96be",
+        color: "828282",
         uniqsnd: !0
     }, {
         name: "6_cribasse",
-        color: "825fd2",
-        uniqsnd: !0
+        color: "828282",
+        uniqsnd: !1
     }, {
         name: "7_distotut",
-        color: "825fd2",
+        color: "828282",
         uniqsnd: !0
     }, {
         name: "8_screw",
-        color: "825fd2",
-        uniqsnd: !0
+        color: "828282",
+        uniqsnd: !1
     }, {
         name: "9_shaolin",
-        color: "825fd2",
+        color: "828282",
         uniqsnd: !0
     }, {
         name: "10_shower",
-        color: "825fd2",
-        uniqsnd: !0
+        color: "828282",
+        uniqsnd: !1
     }, {
         name: "11_basse",
-        color: "e11419",
-        uniqsnd: !0
+        color: "828282",
+        uniqsnd: !1
     }, {
         name: "12_hou",
-        color: "e11419",
-        uniqsnd: !0
+        color: "828282",
+        uniqsnd: !1
     }, {
         name: "13_clav",
-        color: "e11419",
-        uniqsnd: !0
+        color: "828282",
+        uniqsnd: !1
     }, {
         name: "14_synth",
-        color: "e11419",
-        uniqsnd: !0
+        color: "828282",
+        uniqsnd: !1
     }, {
         name: "15_yah",
-        color: "e11419",
-        uniqsnd: !0
+        color: "828282",
+        uniqsnd: !1
     }, {
         name: "16_hurry",
-        color: "f06400",
-        uniqsnd: !0
+        color: "828282",
+        uniqsnd: !1
     }, {
         name: "17_good",
-        color: "f06400",
-        uniqsnd: !0
+        color: "828282",
+        uniqsnd: !1
     }, {
         name: "18_mind",
-        color: "f06400",
-        uniqsnd: !0
+        color: "828282",
+        uniqsnd: !1
     }, {
         name: "19_haha",
-        color: "f06400",
-        uniqsnd: !0
+        color: "828282",
+        uniqsnd: !1
     }, {
         name: "20_wow",
-        color: "f06400",
-        uniqsnd: !0
+        color: "828282",
+        uniqsnd: !1
     }], this.bonusarray = [{
-        name: "Alive",
+        name: "Satisfied",
         src: "v6-b1-alive-hb.mp4",
         code: "4,6,9,14,18",
         sound: "bonus-alive",
         aspire: "aspire-alive"
     }, {
-        name: "Busta",
+        name: "Little Miss",
         src: "v6-b2-busta-hb.mp4",
         code: "1,2,8,11,16",
         sound: "bonus-busta",
         aspire: "aspire-busta"
     }, {
-        name: "VR",
+        name: "Why this world",
         src: "v6-b3-vr-hb.mp4",
         code: "3,8,12,11,20",
         sound: "bonus-vr",
